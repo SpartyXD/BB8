dd5b41e6143ebf7e1d4f53317be62dab39a2c6ee
Mati Oteiza <pachoncitorby@gmail.com>
2025-05-03 18:28:50 -0400

    add bluetooth extra functions

--- PlatformIO Project/src/main.cpp
+++ PlatformIO Project/src/main.cpp
@@ -1,31 +1,39 @@
 #include <Arduino.h>
 #include <objects.h>
 #include <BluetoothSerial.h>
+#include <Preferences.h>
 
 //Globals
 MotorShield motors;
 PassiveBuzzer speaker; 
 BluetoothSerial server;
+Preferences data;
 
-#define NOMBRE_BLUETOOTH "BB8 De Potro"
-#define SPEED 200
+#define NOMBRE_BLUETOOTH "R2D2 De Potro"
+int LEFT_SPEED = 250;
+int RIGHT_SPEED = 250;
 
 
 //==========================================
 //Functions
-void handleCommand(char command);
-
+void handleCommand(String command);
+void spin();
+void handleConfig(String command);
 
 //==========================================
 //MAIN PROGRAM
 
 void setup(){
   Serial.begin(115200);
+  data.begin("R2D2");
   server.begin(NOMBRE_BLUETOOTH);
   delay(500);
 
   motors.init(STNDBY_PIN, PWM_A, A1_PIN, A2_PIN, PWM_B, B1_PIN, B2_PIN);
   speaker.init(SPK);
+
+  LEFT_SPEED = data.getInt("L", 250);
+  RIGHT_SPEED = data.getInt("R", 250);
 
   Serial.println("Systems ready!\n");
   speaker.startupBeep();
@@ -34,14 +42,20 @@ void setup(){
 
 void loop(){
   while(server.available()){
-    handleCommand(server.read());
+    handleCommand(server.readStringUntil('\n'));
   }
 }
 
 //==========================================
 //Implementations
 
-void handleCommand(char letter){
+void handleCommand(String command){
+  if(command.length() > 1){
+    handleConfig(command);
+    return;
+  }
+
+  char letter = command[0];
   //STOP
   if(letter == 'S'){
       motors.stopMotors();
@@ -52,23 +66,54 @@ void handleCommand(char letter){
   switch (letter)
   {
   case 'F':
-      motors.controlMotors(SPEED, SPEED);
+      motors.controlMotors(LEFT_SPEED, RIGHT_SPEED);
       Serial.println("Forward!\n");
       break;
     case 'B':
-      motors.controlMotors(-SPEED, -SPEED);
+      motors.controlMotors(-LEFT_SPEED, -RIGHT_SPEED);
       Serial.println("Backwards!\n");
       break;
     case 'L':
-      motors.controlMotors(-SPEED, SPEED);
+      motors.controlMotors(-LEFT_SPEED, RIGHT_SPEED);
       Serial.println("Turning Left!\n");
       break;
     case 'R':
-      motors.controlMotors(SPEED, -SPEED);
+      motors.controlMotors(LEFT_SPEED, -RIGHT_SPEED);
       Serial.println("Turning right!\n");
+      break;
+    case 'X':
+      speaker.alarmBeep();
+      break;
+    case 'Z':
+      spin();
+      break;
+    case 'Y':
+      speaker.successBeep();
       break;
   //------------------------
   default:
       break;
+  }
+}
+
+
+void spin(){
+  motors.controlMotors(LEFT_SPEED, -RIGHT_SPEED);
+  delay(1250);
+  motors.stopMotors();
+}
+
+void handleConfig(String command){
+  char letter = command[0];
+  int split = command.indexOf(" ");
+
+  if(letter == 'M'){
+    LEFT_SPEED = command.substring(1, split).toInt();
+    RIGHT_SPEED = command.substring(split+1).toInt();
+
+    data.putInt("L", LEFT_SPEED);
+    data.putInt("R", RIGHT_SPEED);
+
+    Serial.println("Received motor speeds: " + String(LEFT_SPEED) + " " + String(RIGHT_SPEED));
   }
 }
